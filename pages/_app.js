53b8ea997e2458d38fdd0a992a44543aa9ecc54c
emilie <emi.m.humbert@googlemail.com>
2020-12-15 19:30:21 +0100

    Add navigation to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+import Head from "next/head";
+import Navigation from '../components/navigation'
+
+import "tailwindcss/tailwind.css";
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Villa les pieds rouges</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <Navigation />
+    <Component {...pageProps} />
+  </>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,9 @@
-import Head from "next/head";
-import "tailwindcss/tailwind.css";
-
-import Navigation from "../components/navigation";
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Villa les pieds rouges</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <main>
-        <Navigation />
-        <div>
-          <img src="/images/beach-view.jpeg" alt="beach-hossegor" />
-        </div>
-      </main>
-
-      <footer></footer>
-    </>
+    <main>
+      <div>
+        <img src="/images/beach-view.jpeg" alt="beach-hossegor" />
+      </div>
+    </main>
   );
 }
