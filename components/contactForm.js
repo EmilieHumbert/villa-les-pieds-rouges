eb638bdb7bc172a2369e8c5f28da887dfcc4ac50
emilie <emi.m.humbert@googlemail.com>
2020-12-21 11:27:39 +0100

    Fix axios post request

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
@@ -8,45 +8,53 @@ export default function ContactForm() {
   });
   const { isSubmitSuccessful, isSubmitting } = formState;
 
-  const onSubmit = async (data) => {
+  const [complete, setcomplete] = useState(false);
+
+  const onSubmit = (data) => {
     axios
-      .get("https://formspree.io/f/xeqprbrl")
-      .then((response) => {
-        console.log("Thank you,your request has been submitted.");
-      })
+      .post("https://formspree.io/f/xeqprbrl", data)
+      .then((response) => setcomplete(true))
       .catch((error) => console.log("error"));
   };
   return (
     <main>
       <h1>Contact us</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="fullName">Full Name</label>
-        <input name="fullName" ref={register({ required: true })} type="text" />
-        {errors.fullName && <p>This is required</p>}
-        <label htmlFor="email">Email</label>
-        <input
-          name="_replyto"
-          ref={register({ required: true })}
-          type="email"
-        />
-        {errors._replyto && <p>This is required</p>}
-        <label htmlFor="message">Message</label>
-        <textarea name="message" ref={register({ required: true })} />
-        {errors.message && <p>This is required</p>}
-        <input
-          name="_subject"
-          ref={register}
-          type="hidden"
-          value="New message from villaLesPiedsRouges.fr"
-        ></input>
-        <button type="submit" disabled={isSubmitting || isSubmitSuccessful}>
-          {isSubmitting
-            ? "Submitting..."
-            : isSubmitSuccessful
-            ? "Submitted"
-            : "Submit"}
-        </button>
-      </form>
+      {complete ? (
+        <div>Done!</div>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            name="fullName"
+            ref={register({ required: true })}
+            type="text"
+          />
+          {errors.fullName && <p>This is required</p>}
+          <label htmlFor="email">Email</label>
+          <input
+            name="_replyto"
+            ref={register({ required: true })}
+            type="email"
+          />
+          {errors._replyto && <p>This is required</p>}
+          <label htmlFor="message">Message</label>
+          <textarea name="message" ref={register({ required: true })} />
+          {errors.message && <p>This is required</p>}
+          <input
+            name="_subject"
+            ref={register}
+            type="hidden"
+            value="New message from villaLesPiedsRouges.fr"
+          ></input>
+          <button type="submit" disabled={isSubmitting || isSubmitSuccessful}>
+            {isSubmitting
+              ? "Submitting..."
+              : isSubmitSuccessful
+              ? "Submitted"
+              : "Submit"}
+          </button>
+        </form>
+      )}
     </main>
   );
 }
