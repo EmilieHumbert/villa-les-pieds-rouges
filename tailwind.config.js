4a9bc9ead6e75478f7d578866dfaa2d13ae96f7f
emilie <emi.m.humbert@googlemail.com>
2020-12-23 20:43:11 +0100

    Add images as background images in the module

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        bedroom: "url('/images/bedroom.jpeg')",
+        beachView: 'url("/images/beach-view.jpeg")',
+        houses: 'url("/images/houses.jpeg")',
+        livingroom: 'url("/images/livingroom.jpeg")',
+        ocean: 'url("/images/ocean.jpeg" )',
+        stairs: 'url("/images/stairs.jpeg")',
+      }),
+    },
+  },
   variants: {
     extend: {
       fontWeight: ["hover"],
