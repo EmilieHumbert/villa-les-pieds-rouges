643a6d5bd2a12a807eca7477c2e89252d26de9f0
emilie <emi.m.humbert@googlemail.com>
2020-12-28 15:55:56 +0100

    Add breakpoint and remove margin

--- components/myCarousel.js
+++ components/myCarousel.js
@@ -7,22 +7,28 @@ export default function MyCarousel() {
   const settings = {
     dots: true,
     className: "center",
-    centerMode: true,
     infinite: true,
-    centerPadding: "100px",
     autoplay: true,
     speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 1,
     pauseOnHover: true,
+    initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
     ],
@@ -32,22 +38,22 @@ export default function MyCarousel() {
     <div>
       <Slider {...settings}>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-bedroom bg-cover bg-center"></div>
+          <div className="h-96 w-full bg-bedroom bg-cover bg-center"></div>
         </div>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-beachView bg-cover bg-center"></div>
+          <div className="h-96 w-full bg-beachView bg-cover bg-center"></div>
         </div>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-houses bg-cover bg-bottom"></div>
+          <div className="h-96 w-full bg-houses bg-cover bg-bottom"></div>
         </div>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-livingroom bg-cover bg-center"></div>
+          <div className="h-96 w-full bg-livingroom bg-cover bg-center"></div>
         </div>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-ocean bg-cover bg-center"></div>
+          <div className="h-96 w-full bg-ocean bg-cover bg-center"></div>
         </div>
         <div className="px-2">
-          <div className="m-10 h-96 w-full bg-stairs bg-cover bg-center"></div>
+          <div className="h-96 w-full bg-stairs bg-cover bg-center"></div>
         </div>
       </Slider>
     </div>
