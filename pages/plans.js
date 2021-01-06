2bd35e8cfeb1bc342a3ff8bcfb6ebf086ca856da
emilie <emi.m.humbert@googlemail.com>
2021-01-06 19:46:02 +0100

    Make Plan layout similar to other pages

--- pages/plans.js
+++ pages/plans.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
 
+import cx from "classnames";
+
 export default function Plans() {
   const [active, setActive] = useState(0);
 
   return (
     <main>
-      <div className="px-auto grid grid-cols-3 gap-4">
-        <div className="selectSection h-xl w-xl bg-fullHouse bg-contain bg-center bg-no-repeat relative">
+      <div className="container gap-32 grid grid-cols-1 lg:grid-cols-3 max-w-xl lg:max-w-4xl mx-auto">
+        <div className="bg-contain bg-fullHouse bg-no-repeat bg-top col-span-1 h-xl relative w-xl">
           <button
             onClick={() => setActive(2)}
-            className="absolute w-2/3 h-40 top-28 left-28 border-double border-4 border-red-900 border-opacity-50 rounded-lg bg-white bg-opacity-25"
+            className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 h-16 left-6 top-8 rounded-lg w-4/5"
           ></button>
           <button
             onClick={() => setActive(1)}
-            className="absolute w-2/3 h-40 top-72 left-28 border-double border-4 border-red-900 border-opacity-50 rounded-lg bg-white bg-opacity-25"
+            className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 h-16 left-6 top-24 rounded-lg w-4/5"
           ></button>
           <button
             onClick={() => setActive(0)}
-            className="absolute w-2/3 h-40 bottom-24 left-28 border-double border-4 border-red-900 border-opacity-50 rounded-lg bg-white bg-opacity-25"
+            className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 h-16 left-6 top-40 rounded-lg w-4/5"
           ></button>
         </div>
-        <div className="contentSection">
+        <div className="col-span-2 contentSection">
           {active === 2 && (
-            <div className="content col-span-2 h-xl w-xl bg-second bg-contain bg-center bg-no-repeat"></div>
+            <div className="bg-second bg-contain bg-top bg-no-repeat col-span-2 content h-xl w-xl"></div>
           )}
           {active === 1 && (
-            <div className="content col-span-2 h-xl w-xl bg-first bg-contain bg-center bg-no-repeat"></div>
+            <div className="bg-first bg-contain bg-top bg-no-repeat col-span-2 content h-xl w-xl"></div>
           )}
           {active === 0 && (
-            <div className="content col-span-2 h-xl w-xl bg-ground bg-contain bg-center bg-no-repeat"></div>
+            <div className="bg-ground bg-contain bg-top bg-no-repeat col-span-2 content h-xl w-xl"></div>
           )}
         </div>
       </div>
