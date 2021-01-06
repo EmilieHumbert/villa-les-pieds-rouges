8b51bc3ee897a43556232ad2ee6fad8d9046aa2e
emilie <emi.m.humbert@googlemail.com>
2021-01-06 16:23:43 +0100

    Add label to form and change button

--- components/contactForm.js
+++ components/contactForm.js
@@ -29,28 +29,37 @@ export default function ContactForm() {
             <div className="col-span-2">
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
+                  <label className="block font-bold mb-2 tracking-wide text-sm uppercase">
+                    Full Name
+                  </label>
                   <input
-                    className="appearance-none focus:outline-none focus:shadow-outline leading-tight px-3 py-2 rounded shadow text-gray-700 w-full"
+                    className="appearance-none focus:outline-none focus:shadow-outline border border-gray-300 leading-tight px-3 py-2 rounded text-gray-700 w-full"
                     name="fullName"
-                    placeholder="Full Name"
+                    placeholder="John Smith"
                     ref={register({ required: true })}
                     type="text"
                   />
                   {errors.fullName && <p>This is required</p>}
                 </div>
                 <div className="mb-6">
+                  <label className="block uppercase tracking-wide text-sm font-bold mb-2">
+                    Email
+                  </label>
                   <input
-                    className="appearance-none focus:outline-none focus:shadow-outline leading-tight px-3 py-2 rounded shadow text-gray-700 w-full"
+                    className="appearance-none focus:outline-none focus:shadow-outline border border-gray-300 leading-tight px-3 py-2 rounded text-gray-700 w-full"
                     name="_replyto"
-                    placeholder="Email"
+                    placeholder="johnSmith@awesome.com"
                     ref={register({ required: true })}
                     type="email"
                   />
                   {errors._replyto && <p>This is required</p>}
                 </div>
                 <div className="mb-6">
+                  <label className="block uppercase tracking-wide text-sm font-bold mb-2">
+                    Message
+                  </label>
                   <textarea
-                    className="appearance-none focus:outline-none focus:shadow-outline leading-tight px-3 py-2 rounded text-gray-700 shadow w-full"
+                    className="appearance-none focus:outline-none focus:shadow-outline border border-gray-300 leading-tight px-3 py-2 rounded text-gray-700 w-full"
                     name="message"
                     placeholder="Message"
                     ref={register({ required: true })}
@@ -65,7 +74,7 @@ export default function ContactForm() {
                 ></input>
                 <div className="flex items-center justify-between">
                   <button
-                    className="bg-red-900 focus:outline-none focus:shadow-outline font-bold hover:bg-blue-700 mb-8 px-4 py-2 rounded text-white"
+                    className="border-2 border-red-900 text-red-900 font-bold hover:bg-red-900 hover:text-white mb-8 px-4 py-2 rounded text-white"
                     type="submit"
                     disabled={isSubmitting || isSubmitSuccessful}
                   >
@@ -81,6 +90,10 @@ export default function ContactForm() {
             <div className="border-b-2 border-red-900 col-span-1 lg:row-start-1 lg:col-start-3 font-bold mb-8 my-2 text-2xl w-md lg:w-sm">
               <h1>Details</h1>
             </div>
+            <p className="leading-normal mb-8 text-xl tracking-wide">
+              Please send us a message if you have any questions. Mathieu
+              Pfiffer will be happy to get back to you as soon as possible.
+            </p>
           </>
         )}
       </div>
