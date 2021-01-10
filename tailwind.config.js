506b27dc9c51c006c96c9b5909c588dfd78b481c
emilie <emi.m.humbert@googlemail.com>
2021-01-10 19:12:12 +0100

    Add background classes to tailwind purge safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: {
+    content: ["./pages/**/*.js", "./components/**/*.js"],
+    options: {
+      safelist: [/^bg-/],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
