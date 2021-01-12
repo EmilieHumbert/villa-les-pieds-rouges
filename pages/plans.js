7a579e95dca799d9508dba9cfb82b497d18ea429
emilie <emi.m.humbert@googlemail.com>
2021-01-12 20:20:53 +0100

    Resizing of buttons on house's plan

--- pages/plans.js
+++ pages/plans.js
@@ -18,35 +18,35 @@ function Plans({ t }) {
           <p className="col-span-2 leading-normal mb-8 text-xl tracking-wide">
             {t("house-paragraph")}
           </p>
-          <div className="bg-contain bg-fullHouse bg-no-repeat bg-top col-span-2 h-xl mb-8 relative w-xl">
+          <div className="bg-contain bg-fullHouse bg-no-repeat bg-top col-span-2 h-72 sm:h-xl mb-8 relative :w-xl">
             <button
               onClick={() => setActive(2)}
-              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-1/6 top-1/6 rounded-lg w-8/12"
+              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-1/6 rounded-lg w-2/6 sm:w-8/12"
             ></button>
             <button
               onClick={() => setActive(1)}
-              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-1/6 top-42 rounded-lg w-8/12"
+              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-42 rounded-lg w-2/6 sm:w-8/12"
             ></button>
             <button
               onClick={() => setActive(0)}
-              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-1/6 rounded-lg w-8/12"
+              className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-35 sm:left-1/6 rounded-lg w-2/6 sm:w-8/12"
             ></button>
             {active === 2 && (
               <button
                 onClick={() => setActive(2)}
-                className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-1/6 top-1/6 rounded-lg w-8/12"
+                className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-1/6 rounded-lg w-2/6 sm:w-8/12"
               ></button>
             )}
             {active === 1 && (
               <button
                 onClick={() => setActive(1)}
-                className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-1/6 top-42 rounded-lg w-8/12"
+                className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-42 rounded-lg w-2/6 sm:w-8/12"
               ></button>
             )}
             {active === 0 && (
               <button
                 onClick={() => setActive(0)}
-                className="absolute bg-opacity-50 bg-white border-47 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-1/6 rounded-lg w-8/12"
+                className="absolute bg-opacity-50 bg-white border-47 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-35 sm:left-1/6 rounded-lg w-2/6 sm:w-8/12"
               ></button>
             )}
           </div>
