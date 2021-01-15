3ae4d31425787a05c3d7db8e01a0c883ca5f5a09
emilie <emi.m.humbert@googlemail.com>
2021-01-15 18:03:16 +0100

    Delete footer and LanguageButton from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Head from "next/head";
 import Navigation from "../components/navigation";
-import Footer from "../components/footer";
 
 import App from "next/app";
 import { appWithTranslation } from "../i18n";
 
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
-import LanguageButton from "../components/languageButton";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -16,14 +14,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Villa les pieds rouges</title>
       </Head>
-      <div className={"hidden sm:block"}>
-        <LanguageButton />
-      </div>
       <Navigation />
       <Component {...pageProps} />
-      <div className={"sm:hidden"}>
-        <Footer />
-      </div>
     </>
   );
 }
