f63e518e0c709281e8c628da4dc04ba4b1217204
emilie <emi.m.humbert@googlemail.com>
2021-01-20 19:47:01 +0100

    Delete un-necessary curly braces

--- components/navigation.js
+++ components/navigation.js
@@ -14,7 +14,7 @@ function Navigation({ t, i18n }) {
   const [active, setActive] = useState(false);
 
   return (
-    <nav className={"mx-7px"}>
+    <nav className="mx-7px">
       <div className="relative flex items-end justify-between mx-auto max-w-xl lg:max-w-4xl mt-m sm:mt-20 mt-8 mb-8 sm:mb-32 space-x-2 sm:space-x-18 lg:space-x-30">
         <Link href="/">
           <a className="bg-red-900 hover:bg-red-800 font-sans h-24 sm:h-36 p-2 text-md sm:text-3xl text-white w-24 min-w-6rem sm:w-36 ">
