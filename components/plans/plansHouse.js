fbde4bfdd9f9ea6361485393d08fdc0298e6d830
emilie <emi.m.humbert@googlemail.com>
2021-05-05 21:42:41 +0200

    New layout for the plans page

--- components/plans/plansHouse.js
+++ components/plans/plansHouse.js
@@ -2,59 +2,38 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 import { withTranslation } from "../../i18n";
-import PlansDetails from "./plansDetails";
-import PlansFloor from "./plansFloor";
+import classNames from "classnames";
 
-function HousePlans({ t }) {
-  const [active, setActive] = useState(0);
-
+function HousePlans({ t, active, setActive }) {
   return (
-    <div className="gap-x-32 grid grid-cols-1 lg:grid-cols-3 max-w-xl lg:max-w-4xl mx-auto">
+    <>
       <h1 className="border-b-2 border-red-900 col-span-2 font-bold mb-8 text-2xl">
         {t("house-title")}
       </h1>
       <p className="col-span-2 leading-normal mb-8 text-xl tracking-wide">
         {t("house-paragraph")}
       </p>
-      <div className="bg-contain bg-fullHouse bg-no-repeat bg-top col-span-2 h-72 sm:h-xl mb-8 relative :w-xl">
-        <button
-          onClick={() => setActive(2)}
-          className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-1/6 rounded-lg w-2/6 sm:w-8/12"
-        ></button>
-        <button
-          onClick={() => setActive(1)}
-          className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-42 rounded-lg w-2/6 sm:w-8/12"
-        ></button>
-        <button
-          onClick={() => setActive(0)}
-          className="absolute bg-opacity-25 bg-white border-4 border-double border-opacity-50 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-35 sm:left-1/6 rounded-lg w-2/6 sm:w-8/12"
-        ></button>
-        {active === 2 && (
+      <div className="bg-contain bg-fullHouse bg-no-repeat bg-top col-span-2 h-72 sm:h-72  mb-8 relative md:l-1/3 :w-xl  ">
+        {[
+          [`${t("second-floor")}`, "top-1/6", 2],
+          [`${t("first-floor")}`, "top-42", 1],
+          [`${t("ground-floor")}`, "bottom-1/6", 0],
+        ].map(([label, positionClassName, floorNumber]) => (
           <button
-            onClick={() => setActive(2)}
-            className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-1/6 rounded-lg w-2/6 sm:w-8/12"
-          ></button>
-        )}
-        {active === 1 && (
-          <button
-            onClick={() => setActive(1)}
-            className="absolute bg-opacity-50 bg-white border-47 border-red-900 focus:outline-none h-1/6 left-35 sm:left-1/6 top-42 rounded-lg w-2/6 sm:w-8/12"
-          ></button>
-        )}
-        {active === 0 && (
-          <button
-            onClick={() => setActive(0)}
-            className="absolute bg-opacity-50 bg-white border-47 border-red-900 bottom-1/6 focus:outline-none h-1/6 left-35 sm:left-1/6 rounded-lg w-2/6 sm:w-8/12"
-          ></button>
-        )}
+            onClick={() => setActive(floorNumber)}
+            className={classNames(
+              "absolute h-1/6 left-1/3 lg:left-2 w-2/6 lg:w-48",
+              "bg-opacity-25 bg-white focus:outline-none",
+              "border-4 border-double border-opacity-50 border-red-900 rounded-lg",
+              positionClassName,
+              active === floorNumber ? "bg-opacity-50 border-47" : ""
+            )}
+          >
+            {label}
+          </button>
+        ))}
       </div>
-      <div className="col-span-2 lg:col-span-1 lg:row-start-1 lg:row-span-3 lg:col-start-3 lg:w-sm">
-        <PlansDetails activeState={[active]} />
-      </div>
-      <div className="col-span-2 lg:col-span-3 row-2 contentSection">
-        <PlansFloor activeState={[active]} />
-      </div>
-    </div>
+    </>
   );
 }
 
--- pages/plans.js
+++ pages/plans.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import PropTypes from "prop-types";
 
 import { withTranslation } from "../i18n";
 import HousePlans from "../components/plans/plansHouse";
+import PlansDetails from "../components/plans/plansDetails";
+import PlansFloor from "../components/plans/plansFloor";
 
 function Plans({ t }) {
+  const [active, setActive] = useState(0);
   return (
     <main className="mx-7px">
       <Head title={t("title")} />
-      <HousePlans />
+      <div className="grid grid-cols-1 lg:grid-cols-3 max-w-xl lg:max-w-4xl mx-auto lg:gap-x-32">
+        <div className="col-span-2 lg:col-span-2">
+          <PlansDetails activeState={[active]} />
+        </div>
+        <div className="col-span-2 lg:col-span-1">
+          <HousePlans active={active} setActive={setActive} />
+        </div>
+        <div className="col-span-3">
+          <PlansFloor activeState={[active]} />
+        </div>
+      </div>
     </main>
   );
 }
