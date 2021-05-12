63a3b0ce9210a05fddc93e53a280036c27ab85ba
emilie <emi.m.humbert@googlemail.com>
2021-05-12 14:01:07 +0200

    Add margin bottom on error message

--- components/error.js
+++ components/error.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Error({ message }) {
   return (
-    <div className="rounded border-red-700 text-red-900 bg-red-200 mt-4 p-4 break-words">
+    <div className="rounded border-red-700 text-red-900 bg-red-200 mx-4 p-4 break-words">
       {message}
     </div>
   );
