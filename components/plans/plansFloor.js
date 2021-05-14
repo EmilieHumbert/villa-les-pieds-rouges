9468803fe5be2aea9b41e6c8c83655a04bb1a7c8
emilie <emi.m.humbert@googlemail.com>
2021-05-14 17:11:04 +0200

    Modify plans page layout

--- components/plans/plansFloor.js
+++ components/plans/plansFloor.js
@@ -3,10 +3,12 @@ import Image from "next/image";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 import { useTranslation } from "next-i18next";
+import classNames from "classnames";
 
-export default function PlansFloor({ activeState: [active] }) {
-  const floorNames = ["Ground", "First", "Second"];
+export default function PlansFloor({ activeState: [active], setActive }) {
   const { t } = useTranslation("plans");
+
+  const floorNames = ["Ground", "First", "Second"];
 
   return (
     <>
@@ -14,6 +16,28 @@ export default function PlansFloor({ activeState: [active] }) {
         {t("floor-title")}
       </h1>
       <div className="col-span-2 content">
+        <div className="flex flex-row">
+          {[
+            [`${t("ground-floor")}`, 0],
+            [`${t("first-floor")}`, 1],
+            [`${t("second-floor")}`, 2],
+          ].map(([floorTitle, floorNumber]) => (
+            <div
+              className="flex items-end justify-start mr-8 mb-8"
+              key={floorTitle}
+            >
+              <a
+                onClick={() => setActive(floorNumber)}
+                className={classNames(
+                  "hidden sm:block capitalize hover:text-gray-500 text-lg border-b-2 border-white",
+                  active === floorNumber ? "border-red-900" : ""
+                )}
+              >
+                {floorTitle}
+              </a>
+            </div>
+          ))}
+        </div>
         <Zoom>
           <Image
             alt={`${floorNames[active]} floor plan`}
--- pages/plans.js
+++ pages/plans.js
@@ -3,25 +3,22 @@ import Head from "next/head";
 
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import HousePlans from "../components/plans/plansHouse";
 import PlansDetails from "../components/plans/plansDetails";
 import PlansFloor from "../components/plans/plansFloor";
 
 function Plans() {
   const { t } = useTranslation("plans");
   const [active, setActive] = useState(0);
+
   return (
     <main className="mx-7px">
       <Head title={t("title")} />
       <div className="grid grid-cols-1 lg:grid-cols-3 max-w-xl lg:max-w-4xl mx-auto lg:gap-x-32">
-        <div className="col-span-2 lg:col-span-2">
-          <PlansDetails activeState={[active]} />
+        <div className="col-span-3 mb-8">
+          <PlansFloor activeState={[active]} setActive={setActive} />
         </div>
-        <div className="col-span-2 lg:col-span-1">
-          <HousePlans active={active} setActive={setActive} />
-        </div>
-        <div className="col-span-3">
-          <PlansFloor activeState={[active]} />
+        <div className="col-span-2 lg:col-span-3">
+          <PlansDetails activeState={[active]} setActive={setActive} />
         </div>
       </div>
     </main>
